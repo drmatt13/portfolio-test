663b5ffd0ec1697fc523e78d6986bd7c9383afb4
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2025-02-06 13:45:57 -0500

    2/6

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,65 @@
 "use client";
 
-import { useRouter, usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 import Link from "next/link";
 
 const Navbar = () => {
-  const router = useRouter();
   const pathname = usePathname();
-
-  useEffect(() => {
-    console.log(pathname);
-  }, [pathname]);
 
   return (
     <div className="w-full md:max-w-[92.5vw] md:w-[48rem] h-16 md:h-[6.5rem] flex">
-      <div className="relative w-full md:mt-8 h-10 bg-white/75 md:rounded-lg border-b md:border border-black/25 shadow flex justify-around items-center text-xs /md:text-sm overflow-hidden font-mono">
-        <div
-          className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
-          onClick={() => router.replace("/projects")}
+      <div className="relative w-full md:mt-8 h-10 bg-white/75 md:rounded-lg border-b md:border border-black/25 shadow flex justify-around items-center text-xs overflow-hidden font-mono">
+        <Link
+          href="/projects"
+          passHref
+          className="w-full h-full overflow-hidden"
         >
-          Projects{pathname === "/projects" && "*"}
-        </div>
-        <div
-          className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
-          onClick={() => router.replace("/skills")}
+          <div
+            className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
+          >
+            Projects{pathname === "/projects" && "*"}
+          </div>
+        </Link>
+        <Link href="/skills" passHref className="w-full h-full overflow-hidden">
+          <div
+            className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
+          >
+            Skills{pathname === "/skills" && "*"}
+          </div>
+        </Link>
+        <Link
+          href="/diagrams"
+          passHref
+          className="w-full h-full overflow-hidden"
         >
-          Skills{pathname === "/skills" && "*"}
-        </div>
-        <div
-          className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
-          onClick={() => router.replace("/diagrams")}
+          <div
+            className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
+          >
+            Diagrams{pathname === "/diagrams" && "*"}
+          </div>
+        </Link>
+        <Link
+          href="/about-me"
+          passHref
+          className="w-full h-full overflow-hidden"
         >
-          Diagrams{pathname === "/diagrams" && "*"}
-        </div>
-        <div
-          className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
-          onClick={() => router.replace("/about-me")}
-        >
-          About Me{pathname === "/about-me" && "*"}
-        </div>
+          <div
+            className={`hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
+          >
+            About Me{pathname === "/about-me" && "*"}
+          </div>
+        </Link>
         <Link
           href="https://www.linkedin.com/in/drmatt13"
           passHref
-          className="w-full h-full overflow-hidden"
           target="_blank"
+          className="w-full h-full overflow-hidden"
         >
           <div
             className={`group hover:bg-[#e8ddd8] transition-colors hover:ease-out ease-in hover:duration-0 w-full h-full flex justify-center items-center cursor-pointer`}
           >
-            Contact{pathname === "/components" && "*"}
+            Contact
             <div className="absolute right-0 top-0 group-hover:bg-[#e8ddd8] h-full pointer-events-none w-2 transition-colors ease-out group-hover:ease-in group-hover:duration-0"></div>
           </div>
         </Link>
